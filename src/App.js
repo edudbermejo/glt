741ba6994d22db647c6449b421f083f72161a751
Eduardo David González <edudbermejo@gmail.com>
2018-09-27 18:18:03 +0200

    feat: main layout deployed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">COFFEE geNEROus</h1>
         </header>
+        <div class="main-content-container">
+        </div>
+        <div class="toolbar">
+            <div class="option-container">
+                <input type="radio" id="ny-location" name="location" value="NY" checked/>
+                <label for="ny-location">New York</label>
+            </div>
+            <div class="option-container">
+                <input type="radio" id="dub-location" name="location" value="DUB"/>
+                <label for="dub-location">Dublin</label>
+            </div>
+        </div>
       </div>
     );
   }
